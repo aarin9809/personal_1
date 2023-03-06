ec08c5c365ff422d7bedee8411d6153d6132159b
sylee9809 <sylee9809@naver.com>
2023-03-06 10:31:52 +0900

    백업 테스트

--- 1204.py
+++ 1204.py
@@ -1,6 +1,4 @@
-n = int(input())
-
-
+# n = input()
 #print(n, end="")  #개행 없음을 이용해서 프린트문을 따로 사용하여 출력을 붙이는 방법도 있음
 #
 #if len(n) == 1:
@@ -26,4 +24,19 @@ n = int(input())
 #
 #else:
 #    print("잘못된 입력")
+#################################정수형으로 풀어보기#############################
+n = int(input())
 
+print(n, end="")
+
+if n == 11 or n ==12 or n == 13:
+    print("th")
+
+elif n % 10 == 1:
+    print("st")
+elif n % 10 == 2:
+    print("nd")
+elif n % 10 == 3:
+    print("rd")
+else:
+    print("th")
